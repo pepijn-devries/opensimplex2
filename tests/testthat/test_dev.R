677f8e114d871d3e0a227c4650999c1a7c275ed5
pepijn-devries <peppi2@gmail.com>
2026-03-10 22:02:30 +0100

    Added test workflow

---
+++ tests/testthat/test_dev.R
@@ -0,0 +1,26 @@
+test_that("Source code should not have things on TODO list", {
+  skip_on_ci()
+  skip_on_cran()
+  skip_if(length(unclass(packageVersion("opensimplex2"))[[1]]) > 3,
+          "Skipping during development")
+  
+  expect_false({
+    files_to_check <-
+      list.files(system.file(package = "opensimplex2"),
+                 pattern = "[.]r$|NEWS|DESCRIPTION|README", recursive = TRUE, full.names = TRUE)
+    files_to_check <- files_to_check[!endsWith(files_to_check, "test_dev.r") &
+                                       !endsWith(files_to_check, ".png")]
+    any(
+      unlist(
+        lapply(files_to_check, function(file) {
+          content <- suppressWarnings(readLines(file))
+          result  <- grepl("TODO", content) & !grepl("grepl\\(\"TODO\"", content) & !grepl("on TODO list", content)
+          if (any(result)) {
+            warning(sprintf("File `%s` has items on TODO list at lines `%s`", file, paste(which(result), collapse = "`, `")))
+          }
+          any(result)
+        })
+      )
+    )
+  })
+})
---
+++ tests/testthat/test_simplex.R
@@ -0,0 +1,8 @@
+test_that("simplex2d is reproducible", {
+  ## TODO not sure if this test with doubles calculation is too accurate
+  announce_snapshot_file(name = "simplex2s.rdata")
+  mat <- simplex2d(100, 100, 1)
+  fn <- tempfile(fileext = ".rdata")
+  save(mat, file = fn, compress = TRUE)
+  expect_snapshot_file(fn, "simplex2d.rdata")
+})
