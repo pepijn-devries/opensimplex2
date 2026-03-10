17706555a4c562c8a5dd8ea075b644b4672c94db
pepijn-devries <peppi2@gmail.com>
2026-03-10 22:19:01 +0100

    updated test to make it less nitty gritty

--- tests/testthat/test_simplex.R
+++ tests/testthat/test_simplex.R
@@ -4,5 +4,10 @@ test_that("simplex2d is reproducible", {
   mat <- simplex2d(100, 100, 1)
   fn <- tempfile(fileext = ".rdata")
   save(mat, file = fn, compress = TRUE)
-  expect_snapshot_file(fn, "simplex2d.rdata")
+  expect_snapshot_file(fn, "simplex2d.rdata", compare = \(old, new) {
+    load(old)
+    mat_old <- mat
+    load(new)
+    all((mat - mat_old) < 1e-4)
+  })
 })
