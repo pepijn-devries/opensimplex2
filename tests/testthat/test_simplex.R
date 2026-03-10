9c1bcfb08365b3033f738bcca0da256923ea48cc
pepijn-devries <peppi2@gmail.com>
2026-03-10 23:06:10 +0100

    Forget about snapshot tests

--- tests/testthat/test_simplex.R
+++ tests/testthat/test_simplex.R
@@ -1,13 +1,8 @@
-test_that("simplex2d is reproducible", {
-  ## TODO not sure if this test with doubles calculation is too accurate
-  announce_snapshot_file(name = "simplex2s.rdata")
-  mat <- simplex2d(100, 100, 1)
-  fn <- tempfile(fileext = ".rdata")
-  save(mat, file = fn, compress = TRUE)
-  expect_snapshot_file(fn, "simplex2d.rdata", compare = \(old, new) {
-    load(old)
-    mat_old <- mat
-    load(new)
-    all(abs(mat - mat_old) < 1e-4)
+test_that("simplex2d is acceptable", {
+  expect_true({
+    mat <- simplex2d(100, 100, 1)
+    test1 <- max(abs(mat[-1,] - mat[-100,])) < .2
+    test2 <- all(mat >= -1) && all(mat <= 1)
+    test1 && test2
   })
 })
