a061dff66661f1abd179566d9fc36620cbd74620
pepijn-devries <peppi2@gmail.com>
2026-03-11 22:47:02 +0100

    Use R's internal random generator for seed. Added 4d variant

--- R/simplex.R
+++ R/simplex.R
@@ -3,14 +3,13 @@
 #' TODO
 #' @param width,height TODO
 #' @param frequency TODO
-#' @param seed TODO
 #' @returns TODO
 #' @examples
 #' mat <- simplex2d(100, 100)
 #' image(mat)
 #' @export
 simplex2d <- function(width, height, frequency = 1, seed = 0L) {
-  noise2d_(width, height, frequency, seed)
+  noise2d_(width, height, frequency)
 }
 
 #' TODO
@@ -18,12 +17,25 @@ simplex2d <- function(width, height, frequency = 1, seed = 0L) {
 #' TODO
 #' @param width,height,depth TODO
 #' @param frequency TODO
-#' @param seed TODO
 #' @returns TODO
 #' @examples
 #' mat <- simplex3d(100, 100, 100)
 #' image(mat[,,1])
 #' @export
-simplex3d <- function(width, height, depth, frequency = 1, seed = 0L) {
-  noise3d_(width, height, depth, frequency, seed)
+simplex3d <- function(width, height, depth, frequency = 1) {
+  noise3d_(width, height, depth, frequency)
+}
+
+#' TODO
+#' 
+#' TODO
+#' @param width,height,depth,slice TODO
+#' @param frequency TODO
+#' @returns TODO
+#' @examples
+#' mat <- simplex4d(25, 25, 25, 25)
+#' image(mat[,,1,1])
+#' @export
+simplex4d <- function(width, height, depth, slice, frequency = 1) {
+  noise4d_(width, height, depth, slice, frequency)
 }
--- src/code.cpp
+++ src/code.cpp
@@ -1,11 +1,15 @@
 #include <cpp11.hpp>
 #include "OpenSimplexCleanup.h"
+#include "R_ext/Random.h"
 using namespace cpp11;
 
 #define PERIOD 64
 
 [[cpp11::register]]
-doubles_matrix<> noise2d_(int width, int height, double frequency, long seed) {
+doubles_matrix<> noise2d_(int width, int height, double frequency) {
+  GetRNGstate();
+  uint32_t seed = (uint32_t)(unif_rand() * 4294967296.0);
+  PutRNGstate();
   writable::doubles_matrix<> mat(width, height);
   OpenSimplexEnv *ose = initOpenSimplex();
   OpenSimplexGradients *osg = newOpenSimplexGradients(ose, seed);
@@ -23,7 +27,10 @@ doubles_matrix<> noise2d_(int width, int height, double frequency, long seed) {
 }
 
 [[cpp11::register]]
-sexp noise3d_(int width, int height, int depth, double frequency, long seed) {
+sexp noise3d_(int width, int height, int depth, double frequency) {
+  GetRNGstate();
+  uint32_t seed = (uint32_t)(unif_rand() * 4294967296.0);
+  PutRNGstate();
   writable::doubles d(width*height*depth);
   d.attr("dim") = writable::integers({width, height, depth});
   d.attr("class") = "array";
@@ -36,6 +43,35 @@ sexp noise3d_(int width, int height, int depth, double frequency, long seed) {
                                                          frequency * (i - width/2)/PERIOD,
                                                          frequency * (j - height/2)/PERIOD,
                                                          frequency * (k - depth/2)/PERIOD);
+      }
+    }
+  }
+  freeOpenSimplexGradients(osg);
+  freeOpenSimplex(ose);
+  return d;
+}
+
+[[cpp11::register]]
+sexp noise4d_(int width, int height, int depth, int slice, double frequency) {
+  GetRNGstate();
+  uint32_t seed = (uint32_t)(unif_rand() * 4294967296.0);
+  PutRNGstate();
+  writable::doubles d(width*height*depth*slice);
+  d.attr("dim") = writable::integers({width, height, depth, slice});
+  d.attr("class") = "array";
+  OpenSimplexEnv *ose = initOpenSimplex();
+  OpenSimplexGradients *osg = newOpenSimplexGradients(ose, seed);
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      for (int k = 0; k < depth; k++) {
+        for (int l = 0; l < slice; l++) {
+          d[i + j*width + k*width*height + l*width*height*depth] =
+            noise4_Classic(ose, osg,
+                           frequency * (i - width/2)/PERIOD,
+                           frequency * (j - height/2)/PERIOD,
+                           frequency * (k - depth/2)/PERIOD,
+                           frequency * (l - depth/2)/PERIOD);
+        }
       }
     }
   }
